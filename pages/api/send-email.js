f579509f1986d90932e5554667900a612f66cf78
ariel walter condorpocco blas <usuario@DESKTOP-ELO9P>
2020-09-20 18:32:55 -0300

    email

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -15,10 +15,17 @@ export default async (req, res) => {
         `;
         
         let tranporter = nodemailer.createTransport({
-            service: "gmail",
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
+                type: 'OAuth2',
+                user: 'arielwalconblas@gmail.com',
+                clientId: '834955275944-roejri9e13caldno2ccoso395aem10th.apps.googleusercontent.com',
+                clientSecret: '5t2uIn7hcYOzysNmYaMaiwAJ',
+                refreshToken: '1//040OS-efxRyXrCgYIARAAGAQSNwF-L9IrBZFi0-3Nk1yLf0B589CGfEd_BTUr2kDxv2NZi16HQOuippSRi90ABY9B_RnLmki33qE',
+                accessToken: 'ya29.a0AfH6SMB4tfUUAiAXygvRPyJJDluw9zOC8Np050bdVfSRAo17fbsd21XU2xexWgWU_G_klgW1wxvWVuoDdmU-oDeCalQ3SfNDNHE7toVVhBUgGUPsDxiv_2tPCWEyY-vvd5-GbkyxBCFF6o8_5_OAjEFWeqqk3iA7d8s',
+                // expires: 1484314697598
             }
         })
 
