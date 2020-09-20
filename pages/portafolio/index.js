2769aae93288343c190c84e366254eec94774c0a
ariel walter condorpocco blas <usuario@DESKTOP-ELO9P>
2020-09-20 16:46:55 -0300

    website

--- pages/portafolio/index.js
+++ pages/portafolio/index.js
@@ -46,8 +46,3 @@ export default function Portafolio({ userName }){
         </>
     )
 }
-
-Portafolio.getInitialProps = () =>{
-    return fetch('http://localhost:3000/api/hello')
-        .then(res => res.json())
-}
